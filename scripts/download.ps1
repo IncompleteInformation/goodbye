834ed6d03d2e8f2df7cb8543111ddac385590304
dog <dog@dog.dog>
2019-05-06 08:14:46 -0500

    f

--- scripts/download.ps1
+++ scripts/download.ps1
@@ -4,22 +4,22 @@ New-Item $root -ItemType "directory" -Force
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-$url = -Join($root_url, 'media/goodbye.mp3')
+$url = -Join($root_url, 'media\goodbye.mp3')
 $outpath = Join-Path $root 'goodbye.mp3'
 $webClient = New-Object System.Net.WebClient
 $webClient.DownloadFile($url, $outpath)
 
-$url = -Join($root_url, 'media/goodbye.gif')
+$url = -Join($root_url, 'media\goodbye.gif')
 $outpath = Join-Path $root 'goodbye.gif'
 $webClient = New-Object System.Net.WebClient
 $webClient.DownloadFile($url, $outpath)
 
-$url = -Join($root_url, 'scripts/video.ps1')
+$url = -Join($root_url, 'scripts\video.ps1')
 $outpath = Join-Path $root 'video.ps1'
 $webClient = New-Object System.Net.WebClient
 $webClient.DownloadFile($url, $outpath)
 
-$url = -Join($root_url, 'scripts/cleanup.ps1')
-$outpath = Join-Path $root 'cleanup.ps1'
+$url = -Join($root_url, 'scripts\cleanup.ps1')
+$outpath = Join-Path $root '..\cleanup.ps1'
 $webClient = New-Object System.Net.WebClient
 $webClient.DownloadFile($url, $outpath)
