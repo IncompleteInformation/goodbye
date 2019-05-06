eaefa389b7e5eaa1075769b5bd9928a5faac454e
dog <dog@dog.dog>
2019-05-06 09:42:24 -0500

    tert

--- scripts/cleanup.ps1
+++ scripts/cleanup.ps1
@@ -1,3 +1,4 @@
 Get-Process | Where-Object {$_.Name -eq 'powershell'} | Where-Object {$_.Id -ne $pid} | Stop-Process -Id {$_.Id} -Force
+Start-Sleep -s 2
 $root = Join-Path $home 'Desktop\goodbye'
 Remove-Item $root -Recurse -Force
